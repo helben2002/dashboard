dc17c95c9e6002dfdd27208d33d95f9a0c0500fc
helben2002 <helenebe87@gmail.com>
2025-09-17 13:50:42 +0200

    first commit

---
+++ src/test/java/se/datababes/dashboard/DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package se.datababes.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
